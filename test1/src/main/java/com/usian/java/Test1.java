467c2cdb0fc5d1c02f8eca9fa8e1bf7318a15c44
ZhouSiMing <1718029015@qq.com>
2020-05-13 15:16:57 +0800

    修改Test1

--- test1/src/main/java/com/usian/java/Test1.java
+++ test1/src/main/java/com/usian/java/Test1.java
@@ -1,6 +1,11 @@
 package com.usian.java;
 
 public class Test1 {
-  //github端(其他的同事)的修改
-  System.out.println("师姐你好");
+
+  public static void main(String[] args) {
+
+    //github端(其他的同事)的修改
+    System.out.println("师姐你好");
+  }
+
 }
